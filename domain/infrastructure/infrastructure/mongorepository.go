47320b63e83020e549e97675d2c1ba37d8728363
Cassius Paim <cassius.paim@gmail.com>
2023-04-12 00:26:54 -0300

    Implementing GetByID method at Port repository using mongo db.

---
+++ domain/infrastructure/infrastructure/mongorepository.go
@@ -0,0 +1,96 @@
+package mongodb
+
+import (
+	"context"
+	"errors"
+	"log"
+
+	"github.com/cassiuspaim/portimporter/domain/entities"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+// PortDB is used by implementation for Mongo of PortRepository
+type PortDB struct {
+	Key         string    `bson:"key"`
+	Name        string    `bson:"name"`
+	City        string    `bson:"city"`
+	Country     string    `bson:"country"`
+	Alias       []string  `bson:"alias"`
+	Regions     []string  `bson:"regions"`
+	Coordinates []float64 `bson:"coordinates"`
+	Province    string    `bson:"province"`
+	Timezone    string    `bson:"timezone"`
+	Unlocs      []string  `bson:"unlocs"`
+	Code        string    `bson:"code"`
+}
+
+// Retrieves a PortDB based on entities.Port passed by parameter.
+func (p PortDB) From(port entities.Port) PortDB {
+	return PortDB{
+		Key:         port.ID,
+		Name:        port.Name,
+		City:        port.City,
+		Country:     port.Country,
+		Alias:       port.Alias,
+		Regions:     port.Regions,
+		Coordinates: port.Coordinates,
+		Province:    port.Province,
+		Timezone:    port.Timezone,
+		Unlocs:      port.Unlocs,
+		Code:        port.Code,
+	}
+}
+
+// Retrieves an entities.Port based on the PortDB.
+func (p PortDB) To() entities.Port {
+	return entities.Port{
+		ID:          p.Key,
+		Name:        p.Name,
+		City:        p.City,
+		Country:     p.Country,
+		Alias:       p.Alias,
+		Regions:     p.Regions,
+		Coordinates: p.Coordinates,
+		Province:    p.Province,
+		Timezone:    p.Timezone,
+		Unlocs:      p.Unlocs,
+		Code:        p.Code,
+	}
+}
+
+type PortRepository struct {
+	client       *mongo.Client
+	databaseName string
+}
+
+func NewPortRepository(client *mongo.Client, databaseName string) PortRepository {
+	log.Printf("Database is %s", databaseName)
+
+	return PortRepository{
+		client:       client,
+		databaseName: databaseName,
+	}
+}
+
+func (p PortRepository) GetByID(id string) (*entities.Port, error) {
+	portsCollection := p.client.Database(p.databaseName).Collection("ports")
+
+	var portDB PortDB
+
+	filter := bson.D{{Key: "key", Value: id}}
+	result := portsCollection.FindOne(context.TODO(), filter)
+	err := result.Decode(&portDB)
+
+	if err != nil {
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			return nil, nil
+		}
+
+		return nil, err
+	}
+
+	port := portDB.To()
+
+	return &port, nil
+}
---
+++ domain/infrastructure/infrastructure/mongorepository_test.go
@@ -0,0 +1,102 @@
+package mongodb
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/ory/dockertest/v3"
+	"github.com/ory/dockertest/v3/docker"
+	"github.com/stretchr/testify/assert"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+// #todo remove global variable
+var dbClient *mongo.Client
+
+const MongoInitdbRootUsername = "root"
+const MongoInitdbRootPassword = "password"
+
+func TestMain(m *testing.M) {
+	pool, err := dockertest.NewPool("")
+	if err != nil {
+		log.Fatalf("Could not construct pool: %s", err)
+	}
+
+	err = pool.Client.Ping()
+	if err != nil {
+		log.Fatalf("Could not connect to Docker: %s", err)
+	}
+
+	// pull mongodb docker image for version 5.0
+	resource, err := pool.RunWithOptions(&dockertest.RunOptions{
+		Repository: "mongo",
+		Tag:        "5.0",
+		Env: []string{
+			// username and password for mongodb superuser
+			"MONGO_INITDB_ROOT_USERNAME=root",
+			"MONGO_INITDB_ROOT_PASSWORD=password",
+		},
+	}, func(config *docker.HostConfig) {
+		// set AutoRemove to true so that stopped container goes away by itself
+		config.AutoRemove = true
+		config.RestartPolicy = docker.RestartPolicy{
+			Name: "no",
+		}
+	})
+	if err != nil {
+		log.Fatalf("Could not start resource: %s", err)
+	}
+
+	// exponential backoff-retry, because the application in the container might not be ready to accept connections yet
+	err = pool.Retry(func() error {
+		var err error
+		dbClient, err = mongo.Connect(
+			context.TODO(),
+			options.Client().ApplyURI(
+				fmt.Sprintf("mongodb://root:password@localhost:%s", resource.GetPort("27017/tcp")),
+			),
+		)
+		if err != nil {
+			return err
+		}
+
+		return dbClient.Ping(context.TODO(), nil)
+	})
+
+	if err != nil {
+		log.Fatalf("Could not connect to docker: %s", err)
+	}
+
+	// run tests
+	code := m.Run()
+
+	// When you're done, kill and remove the container
+	if err = pool.Purge(resource); err != nil {
+		log.Fatalf("Could not purge resource: %s", err)
+	}
+
+	// disconnect mongodb client
+	if err = dbClient.Disconnect(context.TODO()); err != nil {
+		panic(err)
+	}
+
+	os.Exit(code)
+}
+
+func TestFindPorts(t *testing.T) {
+	t.Parallel()
+	t.Run("Given an id of a non existing Port When GetByID is invoked Then no error and not Port is expected", func(t *testing.T) {
+		t.Parallel()
+
+		portRepository := NewPortRepository(dbClient, "portsTest")
+
+		port, err := portRepository.GetByID("idunique")
+		assert.Nil(t, port, "Port must not exist at database")
+		assert.NoError(t, err, "Error must not be found")
+	})
+
+}
